61e332a7fe1499f2e4888cec9331dd367bf58193
Giorgos Pasentsis <gpasents@gmail.com>
2022-05-27 19:50:23 +0300

    bugfixes

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,11 +42,11 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">gpasents@gmail.com</a>
+          <a href="mailto:gpasents@gmail.com" className="p-text">gpasents@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+30 6948677023</a>
+          <a href="tel:+30 (69) 486-77023" className="p-text">+30 6948677023</a>
         </div>
       </div>
       {!isFormSubmitted ? (
